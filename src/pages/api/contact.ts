0a16d01ba246308c234d09018ad11cad58392374
Sajjad Ali <sajjuali439@gmail.com>
2026-01-14 18:37:46 +0500

    update

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -115,8 +115,8 @@ export default async function handler(
 
     await transporter.sendMail({
       from: fromUser,
-      to: "sajjuali439@gmail.com",
-      // cc: "afraz@bdata.ca",
+      to: "syed@bdata.ca",
+      cc: "afraz@bdata.ca",
       replyTo: email,
       subject,
       text,
